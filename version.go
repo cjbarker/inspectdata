bbd35710aa444e3905c96472cde76b81201bd27b
CJ Barker <cjbarker@gmail.com>
2018-09-17 09:44:35 -0700

    Inject version and build into code for package rather than relying on compiler flags for binary

---
+++ version.go
@@ -0,0 +1,7 @@
+package inspectdata
+
+// Identify the version of the release
+const (
+	Version = "1.0.1"    // Release: Major.Minor.Hotfix, ex: 1.0.1
+	Build   = "38795575" // Build release dientification, ex: Git Commit Hash SHA1
+)
